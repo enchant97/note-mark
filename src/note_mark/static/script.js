f5a53e2a1c04dedbf2aef37d52ba975f2b48f64b
Leo Spratt <enchant97@users.noreply.github.com>
2021-02-18 21:16:22 +0000

    fix for page scrolling on textarea input

--- src/note_mark/static/script.js
+++ src/note_mark/static/script.js
@@ -3,6 +3,8 @@
  * @param {Element} elem - the element to resize
  */
 function auto_resize_elem(elem){
+    const window_pos = window.pageYOffset;
     elem.style.height = "auto";
     elem.style.height = elem.scrollHeight + "px";
+    window.scroll(window.pageYOffset, window_pos);
 }
