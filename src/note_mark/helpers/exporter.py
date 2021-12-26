10a5f57e898cc4e4562c9dedd18b3fa30e7f1424
Leo Spratt <enchant97@users.noreply.github.com>
2021-12-26 22:22:43 +0000

    rewrite exporter to use tar.gz instead of folder

--- src/note_mark/helpers/exporter.py
+++ src/note_mark/helpers/exporter.py
@@ -1,9 +1,10 @@
-import shutil
+import tarfile
 from dataclasses import asdict, dataclass
 from datetime import datetime
+from io import BytesIO
 from pathlib import Path
 from typing import Optional, overload
-from uuid import UUID
+from uuid import UUID, uuid4
 
 try:
     import rapidjson as json
@@ -89,33 +90,63 @@ async def export_v1(users: Optional[list[User]] = None) -> ExportV1:
     )
 
 
-def exported_v1_to_exports(exported: ExportV1, show_completion: bool = False) -> Path:
-    """
-    Export the exported metadata and copy notes into
-    the export directory under current datetime,
+def _add_v1_content_to_tar(exported: ExportV1, tar: tarfile.TarFile):
+    # add the metadata file
+    meta_dump = json.dumps(asdict(exported), indent=4, default=str).encode()
+    meta_info = tarfile.TarInfo("meta.json")
+    meta_info.size = len(meta_dump)
+    meta_info.mtime = datetime.utcnow().timestamp()
+    tar.addfile(meta_info, BytesIO(meta_dump))
 
-        :param exported: The export metadata
-        :param show_completion: Place a 'done.txt' file in
-                                directory when export is complete
-        :return: Path to where export took place
-    """
-    export_path = get_admin_export_path() / datetime.utcnow().isoformat()
-    exported_notes_path = export_path / "notes"
-    export_path.mkdir()
-    exported_notes_path.mkdir()
-
-    with open(export_path / "meta.json", "wt") as fo:
-        json.dump(asdict(exported), fo, indent=4, default=str)
-
+    # add each note to the file
     for user in exported.users:
         for notebook in user.notebooks:
             for note in notebook.notes:
-                dst = exported_notes_path / (note.uuid.hex + ".md")
+                dst = Path("notes") / (note.uuid.hex + ".md")
                 src = combine_note_path(notebook.uuid, note.uuid)
-                shutil.copyfile(src, dst)
+                tar.add(src, dst)
 
-    if show_completion:
-        with open(export_path / "done.txt", "wt") as fo:
-            pass
+
+def exported_v1_to_exports(exported: ExportV1) -> Path:
+    """
+    Export the exported metadata and copy notes into
+    the export directory under unique filename
+    beginning with 'export', compressing to a tar.gz file
+
+        :param exported: The export metadata
+        :return: Path to where export took place
+    """
+    # make a temp filename and finished filename
+    export_path_temp = get_admin_export_path() / ("export-" + uuid4().hex[:8] + ".tmp")
+    export_path = export_path_temp.with_suffix(".tar.gz")
+
+    with tarfile.open(export_path_temp, "x:gz") as tar:
+        # add content to tar obj
+        _add_v1_content_to_tar(exported, tar)
+
+    # writing to file has finished,
+    # 'unlock' the file by removing .tmp extention
+    export_path_temp.rename(export_path)
 
     return export_path
+
+
+def exported_v1_to_memory(
+        exported: ExportV1,
+        bytes_obj: Optional[BytesIO]) -> BytesIO:
+    """
+    Same as exported_v1_to_exports but
+    instead exports to a BytesIO obj
+
+        :param exported: The export metadata
+        :param bytes_obj: A optionally open stream, defaults to None
+        :return: the bytes stream
+    """
+    if bytes_obj is None:
+        bytes_obj = BytesIO()
+
+    with tarfile.open(bytes_obj, "w:gz") as tar:
+        # add content to tar obj
+        _add_v1_content_to_tar(exported, tar)
+
+    return bytes_obj
