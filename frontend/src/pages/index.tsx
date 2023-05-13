b02bf7bd2d185063a21719d0bbd1b51d18374bc5
Leo Spratt <enchant97@users.noreply.github.com>
2023-05-13 20:13:10 +0100

    hide login when logged in

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,14 +1,17 @@
 import { A } from '@solidjs/router';
-import type { Component } from 'solid-js';
+import { Component } from 'solid-js';
+import { useApi } from '../contexts/ApiProvider';
 
 const Index: Component = () => {
+  const { apiDetails } = useApi()
+
   return (
     <div class="hero min-h-screen bg-base-200">
       <div class="hero-content text-center">
         <div class="max-w-md">
           <h1 class="text-5xl font-bold">Note Mark</h1>
           <p class="py-6">Fancy tag line here.</p>
-          <A href="/login" class="btn btn-primary">Login</A>
+          {!apiDetails().authToken && <A href="/login" class="btn btn-primary">Login</A>}
         </div>
       </div>
     </div>
