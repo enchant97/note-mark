770e984f4ed9d4f71af736650db8aa4013567f56
Leo Spratt <enchant97@users.noreply.github.com>
2023-05-13 20:05:44 +0100

    move drawer component to use outlet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import { Routes, Route } from '@solidjs/router';
-import { Component, lazy } from 'solid-js';
+import { Routes, Route, Outlet, useParams } from '@solidjs/router';
+import { Component, createEffect, lazy } from 'solid-js';
 import Header from './components/header';
 import { useApi } from './contexts/ApiProvider';
 import ProtectedRoute from './components/protected_route';
@@ -7,6 +7,25 @@ import ProtectedRoute from './components/protected_route';
 const Index = lazy(() => import("./pages/index"));
 const Login = lazy(() => import("./pages/login"));
 const Logout = lazy(() => import("./pages/logout"));
+
+const MainApp: Component = () => {
+  return (
+    <div class="drawer drawer-mobile">
+      <input id="main-drawer" type="checkbox" class="drawer-toggle" />
+      <div class="drawer-content">
+        <Header />
+        <Outlet />
+      </div>
+      <div class="drawer-side">
+        <label for="main-drawer" class="drawer-overlay"></label>
+        <ul class="menu p-4 w-80 bg-base-300 text-base-content">
+          <li><a>Sidebar Item 1</a></li>
+          <li><a>Sidebar Item 2</a></li>
+        </ul>
+      </div>
+    </div>
+  );
+}
 
 const App: Component = () => {
   const { apiDetails } = useApi()
@@ -20,24 +39,13 @@ const App: Component = () => {
   }
 
   return (
-    <div class="drawer drawer-mobile">
-      <input id="main-drawer" type="checkbox" class="drawer-toggle" />
-      <div class="drawer-content">
-        <Header />
-        <Routes>
-          <Route path="/" component={Index} />
-          <ProtectedRoute path="/login" redirectPath="/" condition={hasNoAuth} component={Login} />
-          <ProtectedRoute path="/logout" redirectPath="/" condition={hasAuth} component={Logout} />
-        </Routes>
-      </div>
-      <div class="drawer-side">
-        <label for="main-drawer" class="drawer-overlay"></label>
-        <ul class="menu p-4 w-80 bg-base-300 text-base-content">
-          <li><a>Sidebar Item 1</a></li>
-          <li><a>Sidebar Item 2</a></li>
-        </ul>
-      </div>
-    </div>
+    <Routes>
+      <ProtectedRoute path="/login" redirectPath="/" condition={hasNoAuth} component={Login} />
+      <ProtectedRoute path="/logout" redirectPath="/" condition={hasAuth} component={Logout} />
+      <Route path="/" component={MainApp}>
+        <Route path="/" component={Index} />
+      </Route>
+    </Routes>
   );
 };
 
