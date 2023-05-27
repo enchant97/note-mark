4f79852b9c933884dd9a5a384ff445bc2a0df559
Leo Spratt <enchant97@users.noreply.github.com>
2023-05-27 22:32:21 +0100

    add gfm support

--- backend/core/renderer.go
+++ backend/core/renderer.go
@@ -4,8 +4,17 @@ import (
 	"io"
 
 	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
+)
+
+var markdownRenderer = goldmark.New(
+	goldmark.WithExtensions(
+		extension.GFM,
+		extension.DefinitionList,
+		extension.Footnote,
+	),
 )
 
 func MarkdownToHTML(source []byte, w io.Writer) error {
-	return goldmark.Convert(source, w)
+	return markdownRenderer.Convert(source, w)
 }
