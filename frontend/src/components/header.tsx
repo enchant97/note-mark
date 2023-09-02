412ba9e7167896a52b0a4f76edc362adacb079b9
Leo Spratt <enchant97@users.noreply.github.com>
2023-09-02 16:45:37 +0100

    (frontend) handle re-login flow better

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,9 +1,9 @@
-import { Component, For, createEffect, createSignal } from 'solid-js';
-import { useApi } from '../contexts/ApiProvider';
-import { A } from '@solidjs/router';
+import { Component, For, Show, createEffect, createSignal } from 'solid-js';
+import { A, useNavigate } from '@solidjs/router';
 import { useCurrentUser } from '../contexts/CurrentUserProvider';
 import { THEMES, getTheme, setTheme } from '../core/theme_switcher';
 import Icon from './icon';
+import { useApi } from '../contexts/ApiProvider';
 
 const ThemeSwitcher: Component = () => {
   const [currentTheme, setCurrentTheme] = createSignal(getTheme())
@@ -33,29 +33,32 @@ const ThemeSwitcher: Component = () => {
   )
 }
 
-const ProfileDropdownNoAuth = () => {
-  return (
-    <>
-      <li><A href="/login">Login</A></li>
-    </>
-  )
-}
-
-const ProfileDropdownHasAuth = () => {
+const ProfileDropdown = () => {
+  const navigate = useNavigate()
+  const { apiDetails, setApiDetails } = useApi()
   const { user } = useCurrentUser()
 
   return (
-    <>
-      <li class="menu-title"><span>Logged In As: <span class="kbd kbd-sm">{user()?.username}</span></span></li>
-      <li><A href="/profile">My Profile</A></li>
-      <li><A href="/logout">Logout</A></li>
-    </>
+    <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-300 rounded-box w-52">
+      <Show when={user()} fallback={<li>
+        <Show when={apiDetails().authToken} fallback={<A href="/login">Login</A>}>
+          <button onclick={() => {
+            setApiDetails({ authToken: undefined })
+            navigate("/login")
+          }}>Re-Login</button>
+        </Show>
+      </li>} keyed>
+        {user => <>
+          <li class="menu-title"><span>Logged In As: <span class="kbd kbd-sm">{user.username}</span></span></li>
+          <li><A href="/profile">My Profile</A></li>
+          <li><A href="/logout">Logout</A></li>
+        </>}
+      </Show>
+    </ul>
   )
 }
 
 const Header: Component = () => {
-  const { apiDetails } = useApi()
-
   return (
     <div class="w-full navbar bg-base-100">
       <div class="flex-none lg:hidden">
@@ -71,9 +74,7 @@ const Header: Component = () => {
           <label tabindex="0" class="btn btn-ghost btn-circle shadow-lg">
             <Icon name="user" />
           </label>
-          <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-300 rounded-box w-52">
-            {apiDetails().authToken && <ProfileDropdownHasAuth /> || <ProfileDropdownNoAuth />}
-          </ul>
+          <ProfileDropdown />
         </div>
       </div>
     </div>
