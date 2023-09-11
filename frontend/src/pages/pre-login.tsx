c39753ba6468796b466bcf472b1aca3a31aa5702
Leo Spratt <enchant97@users.noreply.github.com>
2023-09-11 17:01:13 +0100

    (frontend) handle if api is invalid on page load

--- frontend/src/pages/pre-login.tsx
+++ frontend/src/pages/pre-login.tsx
@@ -1,16 +1,22 @@
 import { Component, onMount } from "solid-js"
 import { useApi } from "../contexts/ApiProvider"
 import { useNavigate } from "@solidjs/router"
-import Api from "../core/api"
+import Api, { ApiError, HttpErrors } from "../core/api"
 import { LoadingScreen } from "../components/loading"
 
 const PreLogin: Component = () => {
-  const { apiDetails, setApiDetails } = useApi()
+  const { apiDetails, setApiDetails, clearDetails } = useApi()
   const navigate = useNavigate()
 
   onMount(async () => {
     let result = await new Api({ apiServer: apiDetails().apiServer }).getServerInfo()
-    if (result instanceof Error) navigate("/login")
+    if (result instanceof ApiError) {
+      if (result.status !== HttpErrors.Unauthorized) {
+        // something odd happened, reset stored details
+        clearDetails()
+      }
+      navigate("/login")
+    }
     else {
       setApiDetails({ info: result })
       history.back()
