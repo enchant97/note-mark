0a0aed4073590e3522db297bec6b8fb967550ff3
Leo Spratt <enchant97@users.noreply.github.com>
2023-12-21 20:31:25 +0000

    wip editor toolbar

--- frontend/src/components/editor.tsx
+++ frontend/src/components/editor.tsx
@@ -1,5 +1,5 @@
 import { EditorView, basicSetup } from "codemirror";
-import { EditorState as InternalEditorState } from "@codemirror/state";
+import { EditorSelection, EditorState as InternalEditorState } from "@codemirror/state";
 import { Component, createEffect, createSignal, onMount } from "solid-js";
 import { SetStoreFunction, Store } from "solid-js/store";
 import Icon from "./icon";
@@ -62,6 +62,19 @@ const Editor: Component<EditorProps> = (props) => {
     save(editor.state)
   }
 
+  const wrapSelectionWith = (content: string) => {
+    let state = editor.state
+    let tx = state.update(state.changeByRange(range => {
+      let currentContent = state.sliceDoc(range.from, range.to)
+      let newContent = `${content}${currentContent}${content}`
+      return {
+        changes: { from: range.from, to: range.to, insert: newContent },
+        range: EditorSelection.range(range.from, range.from + newContent.length)
+      }
+    }))
+    editor.dispatch(tx)
+  }
+
   createEffect(() => {
     let newContent = props.content
     if (editor !== undefined) {
@@ -87,13 +100,27 @@ const Editor: Component<EditorProps> = (props) => {
               onInput(v.state)
             }
           }),
-          keymap.of([{
-            key: "Mod-s", run: () => {
-              if (!props.state.saving)
-                triggerSave()
-              return true
-            }
-          }]),
+          keymap.of([
+            {
+              key: "Mod-s", run: () => {
+                if (!props.state.saving)
+                  triggerSave()
+                return true
+              }
+            },
+            {
+              key: "Mod-b", run: () => {
+                wrapSelectionWith("**")
+                return true
+              }
+            },
+            {
+              key: "Mod-i", run: () => {
+                wrapSelectionWith("*")
+                return true
+              }
+            },
+          ]),
         ],
         doc: props.content,
       }),
@@ -105,31 +132,103 @@ const Editor: Component<EditorProps> = (props) => {
 
   return (
     <>
-      <menu class="menu menu-sm menu-horizontal gap-2 bg-base-200 rounded-md shadow-md p-2 w-full items-center">
-        <li><label class="form-control">
-          <span class="label-text cursor-pointer">Auto Save</span>
-          <input
-            class="toggle toggle-sm"
-            type="checkbox"
-            checked={autoSave()}
-            oninput={(ev) => {
-              let v = ev.currentTarget.checked
-              if (v && props.state.unsaved) { triggerSave() }
-              setAutoSave(v)
-            }}
-          />
-        </label></li>
-        <li><button
-          class="btn btn-sm btn-square btn-outline"
-          disabled={props.state.saving}
-          classList={{ "btn-error": props.state.unsaved }}
-          type="button"
-          title="Save Note"
-          onclick={() => triggerSave()}
-        >
-          {props.state.saving && <span class="loading loading-spinner text-warning"></span>}
-          {!props.state.saving && <Icon name="save" />}
-        </button></li>
+      <menu class="menu menu-sm menu-horizontal flex-nowrap gap-6 bg-base-200 rounded-md shadow-md p-2 w-full items-center">
+        <ul class="menu-horizontal gap-2 flex-nowrap">
+          <li><label class="form-control">
+            <span class="label-text cursor-pointer">Auto Save</span>
+            <input
+              class="toggle toggle-sm"
+              type="checkbox"
+              checked={autoSave()}
+              oninput={(ev) => {
+                let v = ev.currentTarget.checked
+                if (v && props.state.unsaved) { triggerSave() }
+                setAutoSave(v)
+              }}
+            />
+          </label></li>
+          <li><button
+            class="btn btn-sm btn-square btn-outline"
+            disabled={props.state.saving}
+            classList={{ "btn-error": props.state.unsaved }}
+            type="button"
+            title="Save"
+            onclick={() => triggerSave()}
+          >
+            {props.state.saving && <span class="loading loading-spinner text-warning"></span>}
+            {!props.state.saving && <Icon name="save" />}
+          </button></li>
+        </ul>
+        <ul class="menu-horizontal gap-2 flex-nowrap">
+          <li><button
+            class="btn btn-sm btn-square btn-outline"
+            title="Bold"
+            onClick={() => wrapSelectionWith("**")}
+          >
+            <Icon name="bold" />
+          </button></li>
+          <li><button
+            class="btn btn-sm btn-square btn-outline"
+            title="Italic"
+            onClick={() => wrapSelectionWith("*")}
+          >
+            <Icon name="italic" />
+          </button></li>
+          <li><button
+            class="btn btn-sm btn-square btn-outline"
+            title="Header"
+          >
+            <Icon name="hash" />
+          </button></li>
+        </ul>
+        <ul class="menu-horizontal gap-2 flex-nowrap hidden sm:flex">
+          <li><button
+            class="btn btn-sm btn-square btn-outline"
+            title="Block Comment"
+          >
+            <Icon name="chevron-right" />
+          </button></li>
+          <li><button
+            class="btn btn-sm btn-square btn-outline"
+            title="De-Indent"
+          >
+            <Icon name="chevrons-left" />
+          </button></li>
+          <li><button
+            class="btn btn-sm btn-square btn-outline"
+            title="Indent"
+          >
+            <Icon name="chevrons-right" />
+          </button></li>
+        </ul>
+        <ul class="menu-horizontal gap-2 flex-nowrap hidden md:flex">
+          <li><button
+            class="btn btn-sm btn-square btn-outline"
+            title="Insert Link"
+          >
+            <Icon name="link" />
+          </button></li>
+          <li><button
+            class="btn btn-sm btn-square btn-outline"
+            title="Insert Image"
+          >
+            <Icon name="image" />
+          </button></li>
+          <li><button
+            class="btn btn-sm btn-square btn-outline"
+            title="Insert Table"
+          >
+            <Icon name="table" />
+          </button></li>
+        </ul>
+        <ul class="menu-horizontal gap-2 flex-nowrap ml-auto">
+          <li><button
+            class="btn btn-sm btn-square btn-outline"
+            title="Open Help"
+          >
+            <Icon name="help-circle" />
+          </button></li>
+        </ul>
       </menu>
       <div ref={(el) => editorDiv = el}></div>
     </>
