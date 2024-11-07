2f7f170edcf640999d4111a0cf3bad0c60884d9e
Leo Spratt <enchant97@users.noreply.github.com>
2024-11-07 12:14:29 +0000

    (frontend) closes #213; syntax highlighting in code blocks

--- frontend/src/components/note/view_rendered.tsx
+++ frontend/src/components/note/view_rendered.tsx
@@ -1,5 +1,6 @@
-import { Accessor, Component, createResource } from 'solid-js';
+import { Accessor, Component, createEffect, createResource } from 'solid-js';
 import render from '../../core/renderer';
+import hljs from 'highlight.js/lib/common';
 
 type NoteViewRenderedProps = {
   content: Accessor<string>
@@ -8,6 +9,11 @@ type NoteViewRenderedProps = {
 const NoteViewRendered: Component<NoteViewRenderedProps> = (props) => {
 
   const [contentRendered] = createResource(props.content, render)
+
+  createEffect(() => {
+    contentRendered()
+    hljs.highlightAll()
+  })
 
   return (
     <div class="prose max-w-none break-words" innerHTML={contentRendered()}></div>
