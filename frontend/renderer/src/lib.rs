727cd7aeaf1d9f4e9be7d00d6a35c3fb93f79872
Leo Spratt <enchant97@users.noreply.github.com>
2024-11-24 15:07:25 +0000

    (frontend) move render into struct

--- frontend/renderer/src/lib.rs
+++ frontend/renderer/src/lib.rs
@@ -2,9 +2,20 @@ use pulldown_cmark::{html, Options, Parser};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn markdown_to_html(raw: &str) -> String {
-    let mut buf = String::new();
-    let parser = Parser::new_ext(raw, Options::all());
-    html::push_html(&mut buf, parser);
-    buf
+pub struct Renderer {}
+
+#[wasm_bindgen]
+impl Renderer {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    #[wasm_bindgen]
+    pub fn markdown_to_html(&self, raw: &str) -> String {
+        let mut buf = String::new();
+        let parser = Parser::new_ext(raw, Options::all());
+        html::push_html(&mut buf, parser);
+        buf
+    }
 }
--- frontend/src/core/renderer.ts
+++ frontend/src/core/renderer.ts
@@ -1,11 +1,14 @@
 import DOMPurify from 'dompurify';
 
-import { markdown_to_html } from '../../renderer/pkg';
+import { Renderer } from '../../renderer/pkg';
+
+const renderer = new Renderer()
+
 // Render markdown into HTML,
 // will sanitize input to prevent possible XSS attacks
 function render(content: string): string {
   const startTime = performance.now()
-  content = markdown_to_html(content)
+  content = renderer.markdown_to_html(content)
   content = DOMPurify.sanitize(content)
   const endTime = performance.now()
   console.debug(`markdown rendering and sanitization took ${endTime - startTime}ms`)
