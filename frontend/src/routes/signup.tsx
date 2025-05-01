b845336d85bd1af6212e6dbf45a5dc805b03317a
Leo Spratt <enchant97@users.noreply.github.com>
2025-05-01 16:28:16 +0100

    implement #225; remove api picker from frontend
    all further installs expect the frontend and backend to be hosted on the
    same domain/port.

--- frontend/src/routes/signup.tsx
+++ frontend/src/routes/signup.tsx
@@ -1,16 +1,14 @@
-import { Component, Show, createSignal } from 'solid-js';
+import { Component, createSignal } from 'solid-js';
 import { createStore } from "solid-js/store";
 import { useApi } from '../contexts/ApiProvider';
 import { A, useNavigate } from '@solidjs/router';
 import { ToastType, apiErrorIntoToast, useToast } from '../contexts/ToastProvider';
 import { ApiError, HttpErrors } from '../core/api';
-import WithApiSelect from '../components/with_api_select';
-import Icon from '../components/icon';
 import Header from '../components/header';
 import Footer from '../components/footer';
 
 const Signup: Component = () => {
-  const { api, apiDetails } = useApi()
+  const { api } = useApi()
   const { pushToast } = useToast()
   const navigate = useNavigate()
   const [formDetails, setFormDetails] = createStore({
@@ -54,73 +52,65 @@ const Signup: Component = () => {
             <h1 class="text-5xl font-bold">Note Mark</h1>
             <p class="py-6">Create your account here.</p>
             <form onSubmit={onSubmit}>
-              <WithApiSelect>
-                <label class="form-control">
-                  <span class="label label-text">Username</span>
-                  <input
-                    class="input input-bordered"
-                    value={formDetails.username}
-                    oninput={(ev) => { setFormDetails({ username: ev.currentTarget.value }) }}
-                    type="text"
-                    placeholder="e.g. leo"
-                    autocomplete="username"
-                    pattern="[A-Za-z0-9]+"
-                    minlength={3}
-                    maxlength={30}
-                    required
-                  />
-                </label>
-                <label class="form-control">
-                  <span class="label label-text">Full Name</span>
-                  <input
-                    class="input input-bordered"
-                    value={formDetails.name}
-                    oninput={(ev) => { setFormDetails({ name: ev.currentTarget.value }) }}
-                    type="text"
-                    placeholder="e.g. Leo S"
-                    maxlength={128}
-                  />
-                </label>
-                <label class="form-control">
-                  <span class="label label-text">Password</span>
-                  <input
-                    class="input input-bordered"
-                    value={formDetails.password}
-                    oninput={(ev) => { setFormDetails({ password: ev.currentTarget.value }) }}
-                    type="password"
-                    placeholder="e.g. P@ssword123"
-                    autocomplete="new-password"
-                    required
-                  />
-                </label>
-                <label class="form-control">
-                  <span class="label label-text">Password Confirm</span>
-                  <input
-                    class="input input-bordered"
-                    classList={{ "input-error": !passwordsMatch() }}
-                    value={formDetails.passwordConfirm}
-                    oninput={(ev) => { setFormDetails({ passwordConfirm: ev.currentTarget.value }) }}
-                    type="password"
-                    placeholder="e.g. P@ssword123"
-                    autocomplete="new-password"
-                    required
-                  />
-                </label>
-                <Show when={!apiDetails().info}>
-                  <div class="alert my-4">
-                    <Icon name="info" />
-                    <span>No valid server set!</span>
-                  </div>
-                </Show>
-                <div class="join join-vertical w-full mt-5">
-                  <button class="btn join-item btn-primary" disabled={!passwordsMatch() || loading() || !apiDetails().info} type="submit">
-                    {loading() && <span class="loading loading-spinner"></span>}
-                    Create User
-                  </button>
-                  <A class="btn join-item" href="/login">Have An Account?</A>
-                  <A class="btn join-item" href="/" classList={{ "btn-disabled": !apiDetails().info }}>Back Home</A>
-                </div>
-              </WithApiSelect>
+              <label class="form-control">
+                <span class="label label-text">Username</span>
+                <input
+                  class="input input-bordered"
+                  value={formDetails.username}
+                  oninput={(ev) => { setFormDetails({ username: ev.currentTarget.value }) }}
+                  type="text"
+                  placeholder="e.g. leo"
+                  autocomplete="username"
+                  pattern="[A-Za-z0-9]+"
+                  minlength={3}
+                  maxlength={30}
+                  required
+                />
+              </label>
+              <label class="form-control">
+                <span class="label label-text">Full Name</span>
+                <input
+                  class="input input-bordered"
+                  value={formDetails.name}
+                  oninput={(ev) => { setFormDetails({ name: ev.currentTarget.value }) }}
+                  type="text"
+                  placeholder="e.g. Leo S"
+                  maxlength={128}
+                />
+              </label>
+              <label class="form-control">
+                <span class="label label-text">Password</span>
+                <input
+                  class="input input-bordered"
+                  value={formDetails.password}
+                  oninput={(ev) => { setFormDetails({ password: ev.currentTarget.value }) }}
+                  type="password"
+                  placeholder="e.g. P@ssword123"
+                  autocomplete="new-password"
+                  required
+                />
+              </label>
+              <label class="form-control">
+                <span class="label label-text">Password Confirm</span>
+                <input
+                  class="input input-bordered"
+                  classList={{ "input-error": !passwordsMatch() }}
+                  value={formDetails.passwordConfirm}
+                  oninput={(ev) => { setFormDetails({ passwordConfirm: ev.currentTarget.value }) }}
+                  type="password"
+                  placeholder="e.g. P@ssword123"
+                  autocomplete="new-password"
+                  required
+                />
+              </label>
+              <div class="join join-vertical w-full mt-5">
+                <button class="btn join-item btn-primary" disabled={!passwordsMatch() || loading()} type="submit">
+                  {loading() && <span class="loading loading-spinner"></span>}
+                  Create User
+                </button>
+                <A class="btn join-item" href="/login">Have An Account?</A>
+                <A class="btn join-item" href="/">Back Home</A>
+              </div>
             </form>
           </div>
         </div>
