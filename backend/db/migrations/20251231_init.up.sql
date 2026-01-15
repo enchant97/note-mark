d37e31675d4bd58a42b53cffc8e36dd851881cff
Leo Spratt <enchant97@users.noreply.github.com>
2026-01-15 16:50:06 +0000

    add full user db model

--- backend/db/migrations/20251231_init.up.sql
+++ backend/db/migrations/20251231_init.up.sql
@@ -1,12 +1,31 @@
 CREATE TABLE users (
   uid BLOB PRIMARY KEY,
-  username TEXT UNIQUE NOT NULL
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP,
+  username TEXT NOT NULL,
+  password_hash BLOB,
+  name TEXT
 );
+
+CREATE UNIQUE INDEX idx_users_username ON users(username);
+
+CREATE TABLE oidc_users (
+  id INTEGER PRIMARY KEY,
+  user_uid BLOB NOT NULL,
+  user_sub TEXT NOT NULL,
+  provider_name TEXT NOT NULL,
+  FOREIGN KEY (user_uid) REFERENCES users(uid) ON DELETE CASCADE
+);
+
+CREATE UNIQUE INDEX idx_oidc_user ON oidc_users(user_uid, provider_name);
+
+CREATE UNIQUE INDEX idx_oidc_provider ON oidc_users(user_sub, provider_name);
 
 CREATE TABLE tree_cache (
   owner_uid BLOB PRIMARY KEY,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   cache BLOB NOT NULL,
   cache_version INTEGER NOT NULL,
-  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (owner_uid) REFERENCES users(uid) ON DELETE CASCADE
 );
