233c8f57139a71d2580dfbb00a844d29f2e5ccb7
Leo Spratt <enchant97@users.noreply.github.com>
2026-02-23 20:19:45 +0000

    add header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,3 +1,91 @@
-export default function Header(props) {
-  return (<></>)
+import { createEffect, createSignal, For, Show } from "solid-js"
+import { getTheme, setTheme, THEMES } from "~/core/theme-switcher"
+import Icon from "./Icon"
+import { A } from "@solidjs/router"
+import { useSession } from "~/contexts/SessionProvider"
+
+function ThemeSwitcher() {
+  const [currentTheme, setCurrentTheme] = createSignal(getTheme())
+  createEffect(() =>
+    setTheme(currentTheme())
+  )
+
+  return (
+    <details class="dropdown">
+      <summary class="btn flex gap-2">
+        <Icon name="sun" />
+        <Icon name="moon" />
+      </summary>
+      <ul class="mt-2 p-2 menu dropdown-content z-[1] w-52 bg-base-100">
+        <For each={THEMES}>
+          {(theme) => (
+            <li><button
+              onclick={(ev) => {
+                setCurrentTheme(theme.name)
+                ev.currentTarget.closest("details")?.removeAttribute("open")
+              }}
+              classList={{ "active": currentTheme() === theme.name }}
+              type="button"
+            >
+              {theme.title}
+            </button></li>
+          )}
+        </For>
+      </ul>
+    </details>
+  )
+}
+
+function ProfileDropdown() {
+  const { userInfo } = useSession()
+
+  return (
+    <details class="dropdown dropdown-end">
+      <summary class="btn btn-circle"><Icon name="user" /></summary>
+      <menu class="mt-2 p-2 menu dropdown-content z-[1] bg-base-100 w-52">
+        <Show when={userInfo()} fallback={<li><A href="/login">Login</A></li>} keyed>
+          {user => <>
+            <li class="menu-title"><span>Logged In As: <span class="kbd kbd-sm">{user.preferred_username}</span></span></li>
+            <li><A href="/profile">My Profile</A></li>
+            <li><A href="/logout">Logout</A></li>
+          </>}
+        </Show>
+      </menu>
+    </details>
+  )
+}
+
+export default function Header(props: { disableDrawerToggle?: boolean }) {
+  const { userInfo } = useSession()
+
+  return (
+    <div class="sticky top-2 z-10 px-2">
+      <div class="navbar backdrop-glass shadow-glass">
+        <Show when={!props.disableDrawerToggle}>
+          <label for="main-drawer" class="lg:hidden btn btn-square btn-ghost shadow">
+            <Icon name="menu" />
+          </label>
+        </Show>
+        <span class="px-2 mx-2 text-xl hidden sm:block">Note Mark</span>
+        <div class="flex-1"></div>
+        <div class="flex gap-4">
+          <ThemeSwitcher />
+          <A
+            class="btn btn-circle"
+            activeClass="btn-disabled"
+            href="/scratch-pad"
+            title="Scratch Pad"
+          ><Icon name="file-text" /></A>
+          <A
+            title="Home"
+            activeClass="btn-disabled"
+            class="btn btn-circle"
+            end={true}
+            href={(userInfo() ?? null) === null ? "/" : `/${userInfo()?.preferred_username}`}
+          ><Icon name="home" /></A>
+          <ProfileDropdown />
+        </div>
+      </div>
+    </div>
+  );
 }
