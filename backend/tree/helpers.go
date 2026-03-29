6943f06da8054e2ee5a85953ccd0081388f62921
Leo Spratt <enchant97@users.noreply.github.com>
2026-03-29 14:59:43 +0100

    fix node tree filtering
    this does not allow sharing of children when the parent is unshared

--- backend/tree/helpers.go
+++ backend/tree/helpers.go
@@ -9,52 +9,25 @@ import (
 // Filter a node tree by using the AccessControl data.
 func FilteredNodeTree(tree core.NodeTree, username *core.Username) core.NodeTree {
 	filteredMap := core.NodeTree{}
-	for _, node := range tree {
-		if filteredNode := FilteredNode(node, username); filteredNode != nil {
-			filteredMap[node.Slug] = node
-		}
-		if node.FrontMatter.AccessControl != nil {
-			if node.FrontMatter.AccessControl.PublicRead {
-				filteredMap[node.Slug] = node
-			} else if username != nil {
-				if _, exists := node.FrontMatter.AccessControl.Users[*username]; exists {
-					filteredMap[node.Slug] = node
-				}
-			}
+	for slug, node := range tree {
+		if filteredNode := FilteredNode(*node, username); filteredNode != nil {
+			filteredMap[slug] = node
 		}
 	}
 	return filteredMap
 }
 
-func FilteredNode(node *core.Node, username *core.Username) *core.Node {
-	if node == nil {
-		return nil
-	}
-	var filteredNode *core.Node = nil
-	// filter top level node
+func FilteredNode(node core.Node, username *core.Username) *core.Node {
 	if node.FrontMatter.AccessControl != nil {
 		if node.FrontMatter.AccessControl.PublicRead {
-			filteredNode = node
+			return &node
 		} else if username != nil {
 			if _, exists := node.FrontMatter.AccessControl.Users[*username]; exists {
-				filteredNode = node
+				return &node
 			}
 		}
 	}
-	// filter children
-	if filteredNode != nil && filteredNode.Type == "note" {
-		for _, childNode := range filteredNode.Children {
-			if childNode.Type == "note" {
-				newChild := FilteredNode(childNode, username)
-				if newChild == nil {
-					delete(filteredNode.Children, childNode.Slug)
-				} else {
-					filteredNode.Children[childNode.Slug] = newChild
-				}
-			}
-		}
-	}
-	return filteredNode
+	return nil
 }
 
 // Convert the named access control mode into a number.
