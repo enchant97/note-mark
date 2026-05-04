08859294b04a3cb90b6896b9630c98eb7047b771
Leo Spratt <enchant97@users.noreply.github.com>
2026-05-04 11:03:25 +0100

    remove startup ascii art

--- backend/cli/serve.go
+++ backend/cli/serve.go
@@ -33,24 +33,11 @@ func commandServe(
 		return err
 	} else {
 		// Start server
-		fmt.Println(`
-oooo   oooo  ooooooo   ooooooooooo ooooooooooo
- 8888o  88 o888   888o 88  888  88  888    88
- 88 888o88 888     888     888      888ooo8
- 88   8888 888o   o888     888      888    oo
-o88o    88   88ooo88      o888o    o888ooo8888
-
-oooo     oooo      o      oooooooooo  oooo   oooo
- 8888o   888      888      888    888  888  o88
- 88 888o8 88     8  88     888oooo88   888888
- 88  888  88    8oooo88    888  88o    888  88o
-o88o  8  o88o o88o  o888o o888o  88o8 o888o o888o`)
-		fmt.Println()
 		if appConfig.Bind.UnixSocket == "" {
-			fmt.Println("Serving on: http://" + appConfig.Bind.AsAddress())
+			slog.Info(fmt.Sprintf("Serving on http://%s", appConfig.Bind.AsAddress()))
 			return http.ListenAndServe(appConfig.Bind.AsAddress(), mux)
 		} else {
-			fmt.Printf("Serving on '%s'\n", appConfig.Bind.UnixSocket)
+			slog.Info(fmt.Sprintf("Serving on %s", appConfig.Bind.UnixSocket))
 			sock, err := net.Listen("unix", appConfig.Bind.UnixSocket)
 			if err != nil {
 				return err
