bb19837693b8c9202d4dd162ee71d0fc5da3537a
Leo Spratt <enchant97@users.noreply.github.com>
2026-06-12 15:56:20 +0100

    (frontend) fix folder icon rotations

--- frontend/src/components/Icon.tsx
+++ frontend/src/components/Icon.tsx
@@ -31,6 +31,6 @@ export function FileIcon({ }) {
   return <Icon name={"file"} size={16} />
 }
 
-export function FolderIcon({ expand }) {
-  return <Icon name={expand() ? "chevron-down" : "chevron-right"} size={16} />
+export function FolderIcon() {
+  return <Icon name={"chevron-right"} size={16} />
 }
